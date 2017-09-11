bb85f7c25434233acfd4a6fd400958e32baaa058
Connor Nishijima <connornishijima@gmail.com>
2017-09-11 01:22:50 -0600

    Update seeded_tones.ino

--- examples/seeded_tones/seeded_tones.ino
+++ examples/seeded_tones/seeded_tones.ino
@@ -1,3 +1,11 @@
+/*
+  Synth | Seeded Tones Example
+  
+  Synth has a peculiar function built in. Given a number as an input seed, it can generate a
+  random wave shape for your tone! This can be smooth, gritty, unique or earpiercing. Give random
+  numbers a try! Here a tone of 440Hz is seeded with the changing milliseconds since execution began.
+*/
+
 #include "Synth.h"
 Synth syn(9);
 
@@ -8,6 +16,6 @@ void loop() {
   // put your main code here, to run repeatedly:
   uint16_t seed = millis();
   
-  syn.tone_seeded(220, 255, seed);
+  syn.tone_seeded(440, 255, seed);
   delay(200);
 }
